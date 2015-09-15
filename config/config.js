0f978ff2c0c44b79804b1edd40130955bc3de334
Michael Len <michael.len@ooyala.com>
2015-09-14 21:51:51 -0700

    update config to use storytree envvars

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 
 exports.gplus = {
-  clientId:process.env.LENSITE_GPLUS_CLIENT_ID,
-  secret:process.env.LENSITE_GPLUS_SECRET
+  clientId:process.env.STORYTREE_GPLUS_CLIENT_ID,
+  secret:process.env.STORYTREE_GPLUS_SECRET
 };
 
 exports.session = {
-  secretKey:process.env.LENSITE_SESSION_SECRET_KEY
+  secretKey:process.env.STORYTREE_SESSION_SECRET_KEY
 };
 
 exports.mysql = {
-  host:process.env.LENSITE_DB_HOSTNAME,
-  user:process.env.LENSITE_DB_USER,
-  password:process.env.LENSITE_DB_PASSWORD,
-  database:process.env.LENSITE_DB_DATABASE_NAME
+  host:process.env.STORYTREE_DB_HOSTNAME,
+  user:process.env.STORYTREE_DB_USER,
+  password:process.env.STORYTREE_DB_PASSWORD,
+  database:process.env.STORYTREE_DB_DATABASE_NAME
 };
